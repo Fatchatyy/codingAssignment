9032287dbc55605ad3dac4d60bae4a22b4a2b828
Fatchatyy <124646957+Fatchatyy@users.noreply.github.com>
2024-07-09 19:40:09 +0200

    removed comments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,6 @@ import React from 'react';
 
 function Navbar() {
   return (
-//    <nav className="bg-navbar py-6">
-//    <div className="container mx-auto flex justify-between items-center">
-//      <div className="text-2xl font-bold text-logo">SaasTribe</div>
-//      <div className="flex space-x-6 items-center">
-//        <ul className="flex space-x-6 ml-auto">
-//          <li><a href="#" className="hover:text-purchase-button text-navlink">Home</a></li>
-//          <li><a href="#" className="hover:text-purchase-button text-navlink">Company</a></li>
-//          <li><a href="#" className="hover:text-purchase-button text-navlink">Account</a></li>
-//          <li><a href="#" className="hover:text-purchase-button text-navlink">Products</a></li>
-//        </ul>
-//        <button className="bg-purchase-button px-4 py-2 rounded text-purchase-text">Purchase Now</button>
-//      </div>
-//    </div>
-//  </nav>
   <nav className="bg-navbar py-6">
   <div className="container mx-auto flex justify-between items-center">
     <div className="text-2xl font-bold text-logo">SaasTribe</div>
