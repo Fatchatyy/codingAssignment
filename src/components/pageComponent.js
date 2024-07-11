2c98adc1012fee4b53269db4fd1195db899a36df
Fatchatyy <124646957+Fatchatyy@users.noreply.github.com>
2024-07-11 04:07:52 +0200

    added fonts to the body

--- src/components/pageComponent.js
+++ src/components/pageComponent.js
@@ -11,12 +11,12 @@ const pageComponent = () => {
     
     {/* Content of the first column */}
     <div className="absolute inset-0 flex flex-col top-20 items-center left-24 right-10 text-white">
-      <h2 className="text-4xl font-bold z-10 relative leading-tight text-Title ">Ready to help you in your projects!</h2>
+      <h2 className="text-4xl font-bold z-10 relative leading-tight text-Title font-montserrat ">Ready to help you in your projects!</h2>
       <div className="z-10 relative">
-        <p className=" text-xl pt-5 pb-3">Our sales team will get in touch to better understand your needs , and will help you with the sign-up process</p>
+        <p className=" text-xl pt-5 pb-3 font-montserrat font-medium">Our sales team will get in touch to better understand your needs , and will help you with the sign-up process</p>
         <div className="flex mt-4">
-          <button className="bg-purchase-button hover:bg-blue-600 text-lg text-white font-bold py-2 px-4 rounded mr-2">Start Now</button>
-          <button className="bg-navbar border-2 border-contour hover:bg-green-600 text-lg text-white font-bold py-2 px-4 rounded">Contact Sales</button>
+          <button className="bg-purchase-button hover:bg-blue-600 text-lg text-white font-semibold py-2 px-4 rounded font-montserrat mr-2">Start Now</button>
+          <button className="bg-navbar border-2 border-contour hover:bg-green-600 text-lg text-white font-semibold font-montserrat py-2 px-4 rounded">Contact Sales</button>
         </div>
       </div>
     </div>
@@ -29,38 +29,38 @@ const pageComponent = () => {
   {/* Card 1 */}
   <div className="bg-card rounded-lg shadow-md p-4 w-48 h-52 flex flex-col justify-center">
     <img src="/img/ppl.png" className=" h-8 w-8 object-cover rounded-t-lg mb-2 ml-4" />
-    <h2 className="text-xl font-semibold text-white mb-2 mt-2 ">Accounts</h2>
-    <p className="text-sm text-p">Manage an unlimited number of accounts from one place</p>
+    <h2 className="text-xl font-semibold text-white mb-2 mt-2 font-montserrat font-semibold">Accounts</h2>
+    <p className="text-sm text-p font-montserrat font-medium">Manage an unlimited number of accounts from one place</p>
   </div>
   {/* Card 2 */}
   <div className="bg-card rounded-lg shadow-md p-4 w-48 h-52 flex flex-col justify-center">
     <img src="/img/Key.png" className=" h-8 w-8 object-cover rounded-t-lg mb-2 ml-4" />
-    <h2 className="text-xl font-semibold text-white  mt-2">Roles & Permissions</h2>
-    <p className="text-sm text-p">Full Control with flexible user permissions for views and actions</p>
+    <h2 className="text-xl font-semibold text-white font-montserrat font-semibold mt-2">Roles & Permissions</h2>
+    <p className="text-sm text-p font-montserrat font-medium">Full Control with flexible user permissions for views and actions</p>
   </div>
   {/* Card 3 */}
   <div className="bg-card rounded-lg shadow-md p-4 w-48 h-52 flex flex-col justify-center">
     <img src="/img/card.png" className="w-8 h-8 object-cover rounded-t-lg mb-2 ml-4" />
-    <h2 className="text-xl font-semibold text-white mb-2 mt-2">Integration</h2>
-    <p className="text-sm text-p">Connect the tools you already use</p>
+    <h2 className="text-xl font-semibold text-white font-montserrat font-semibold mb-2 mt-2">Integration</h2>
+    <p className="text-sm text-p font-montserrat font-medium">Connect the tools you already use</p>
   </div>
   {/* Card 4 */}
   <div className="bg-card rounded-lg shadow-md p-4 w-48 h-52 flex flex-col justify-center">
     <img src="/img/bot.png" className="w-8 h-8 object-cover rounded-t-lg mb-2 ml-4" />
-    <h2 className="text-xl font-semibold text-white mb-2 mt-2">Chat Bots</h2>
-    <p className="text-sm text-p">AI powered chatbots to improve your business and customer satisfaction</p>
+    <h2 className="text-xl font-semibold text-white font-montserrat font-semibold mb-2 mt-2">Chat Bots</h2>
+    <p className="text-sm text-p font-montserrat font-medium">AI powered chatbots to improve your business and customer satisfaction</p>
   </div>
   {/* Card 5 */}
   <div className="bg-card rounded-lg shadow-md p-4 w-48 h-52 flex flex-col justify-center">
     <img src="/img/msg.png" className="w-8 h-8 object-cover rounded-t-lg mb-2 ml-4" />
-    <h2 className="text-xl font-semibold text-white mb-2 mt-2">In-App Messaging</h2>
-    <p className="text-sm text-p">Build a better business with proactive messages</p>
+    <h2 className="text-xl font-semibold text-white font-montserrat font-semibold mb-2 mt-2">In-App Messaging</h2>
+    <p className="text-sm text-p font-montserrat font-medium">Build a better business with proactive messages</p>
   </div>
   {/* Card 6 */}
   <div className="bg-card rounded-lg shadow-md p-4 w-48 h-52 flex flex-col justify-center">
     <img src="/img/book.png" className="w-8 h-8 object-cover rounded-t-lg mb-2 ml-4" />
-    <h2 className="text-xl font-semibold text-white mb-2 mt-2">Knowledge Base</h2>
-    <p className="text-sm text-p">Create and publish answers for customers</p>
+    <h2 className="text-xl font-semibold text-white font-montserrat font-semibold mb-2 mt-2">Knowledge Base</h2>
+    <p className="text-sm text-p font-montserrat font-medium">Create and publish answers for customers</p>
   </div>
 </div>
 </div>
